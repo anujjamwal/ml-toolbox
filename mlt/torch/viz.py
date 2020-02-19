f5650d43a74b99a7ba8773ee64ce3ca82de25872
Anuj Jamwal <anuj.jamwal1@gmail.com>
2020-02-19 00:05:09 -0500

    Added viz for conv2d weights

--- mlt/torch/viz.py
+++ mlt/torch/viz.py
@@ -29,14 +29,15 @@ def show_conv2d(layer: torch.nn.Conv2d, fig_num_cols=10):
     weights = layer.weight.data.cpu()
     num_kernels = weights.shape[0] * weights.shape[1]
 
-    nrows = 1 + num_kernels
-
+    nrows = 1 + num_kernels//fig_num_cols
+    count = 0
     fig = plt.figure(figsize=(fig_num_cols, nrows))
 
     # looping through all the kernels in each channel
     for i in range(weights.shape[0]):
         for j in range(weights.shape[1]):
-            ax1 = fig.add_subplot(nrows, fig_num_cols, i * 10 + j + 1)
+            count += 1
+            ax1 = fig.add_subplot(nrows, fig_num_cols, count)
             npimg = np.array(weights[i, j].numpy(), np.float32)
             npimg = (npimg - np.mean(npimg)) / np.std(npimg)
             npimg = np.minimum(1, np.maximum(0, (npimg + 0.5)))
